4ae7fca6b2130a2853165f94128d3f99d05d7fb6
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-11 17:20:17 -0300

    Tornando o programa mais intuitivo, agor o usuário escolhe: produto, quantidade e preço.

--- Program.cs
+++ Program.cs
@@ -6,19 +6,24 @@ namespace atacadoOuVarejo
     {
         static void Main(string[] args)
         {
-            string produto = "whisky Black Label";
-            double preco = 100.00;
             
             Console.WriteLine("Boas vindas a loja 1000Coisas!");
-            Console.WriteLine("O que você gostaria de comprar?");
-            Console.WriteLine($"{produto}  R$ {preco}--.");
+            Console.WriteLine("Aqui na 1000Coisas, você pode escoler qualquer produto, qualquer quantidade e qualquer preço que você quiser pagar.");
+            Console.WriteLine("Temos tudo o que você possa imaginar, basta escolher seu produto, quantidade e preço.\n Boas compras!");
+            Console.WriteLine("O que você deseja?");
+             string produto = Console.ReadLine();
+
             Console.WriteLine("Caso queira comprar em grande quantidade, temos os seguintes descontos:");
             Console.WriteLine("Até 10 unidades: valor total ");
             Console.WriteLine("De 11 a 20 unidades: 10% de desconto");
             Console.WriteLine("De 21 a 50 unidades: 20% de desconto ");
             Console.WriteLine("Acima de 50 unidades: 25% de desconto");
-            Console.WriteLine("Quantas unidades você vai querer?");
-            int quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Quantas unidades {produto}, você vai querer?");
+              int quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Aqui não é Casas Bahias, mas vor te perguntar, Vai querer pagar quanto em cada {produto}?");
+            double preco = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{produto}-{quantidade}R$ {preco}");
 
             double subTotal = quantidade * preco;
 
