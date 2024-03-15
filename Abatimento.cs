df845ff11cbfbea44b6fc16f109b838cc18da2b6
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-15 15:46:12 -0300

    Adicionada a class com orientação a objeto

--- Abatimento.cs
+++ Abatimento.cs
@@ -6,6 +6,33 @@ namespace atacadoOuVarejo
 {
     class Abatimento
     {
+        public static void CalcularCompra(string produto, int quantidade, double preco)
+        {
+            Console.WriteLine($"{produto}-{quantidade}R$ {preco}");
 
+            double subTotal = quantidade * preco;
+            double desconto = 0;
+
+            if (quantidade > 10 && quantidade <= 20)
+            {
+                desconto = subTotal * 0.1;
+            }
+            else if (quantidade >= 21 && quantidade <= 50)
+            {
+                desconto = subTotal * 0.2;
+            }
+            else if (quantidade >= 51)
+            {
+                desconto = subTotal * 0.25;
+            }
+
+            double totalAPagar = subTotal - desconto;
+
+            Console.WriteLine($"{produto} Quant: {quantidade} x {preco}");
+            Console.WriteLine($"Sub-total R$: {subTotal}");
+            Console.WriteLine($"Desconto: {desconto}");
+            Console.WriteLine($"Valor total a pagar: {totalAPagar}");
+            Console.WriteLine("Obigado e volte sempre...");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,62 +6,23 @@ namespace atacadoOuVarejo
     {
         static void Main(string[] args)
         {
-            
             Console.WriteLine("Boas vindas a loja 1000Coisas!");
-            Console.WriteLine("Aqui na 1000Coisas, você pode escoler qualquer produto, qualquer quantidade e qualquer preço que você quiser pagar.");
+            Console.WriteLine("Aqui na 1000Coisas, você pode escolher qualquer produto, quantidade e qualquer preço que você quiser pagar.");
             Console.WriteLine("Temos tudo o que você possa imaginar, basta escolher seu produto, quantidade e preço.\n Boas compras!");
             Console.WriteLine("O que você deseja?");
-             string produto = Console.ReadLine();
+            string produto = Console.ReadLine();
 
             Console.WriteLine("Caso queira comprar em grande quantidade, temos os seguintes descontos:");
             Console.WriteLine("Até 10 unidades: valor total ");
             Console.WriteLine("De 11 a 20 unidades: 10% de desconto");
             Console.WriteLine("De 21 a 50 unidades: 20% de desconto ");
             Console.WriteLine("Acima de 50 unidades: 25% de desconto");
-            Console.WriteLine($"Qual a quantidade de {produto}, você vai querer?");
-              int quantidade = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Aqui não é Casas Bahias, mas vor te perguntar, Vai querer pagar quanto em cada {produto}?");
+            Console.WriteLine($"Qual a quantidade de {produto}, você vai levar?");
+            int quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Aqui não é Casas Bahias, mas vor te perguntar, Vai querer pagar quanto em cada {produto}? R$:");
             double preco = double.Parse(Console.ReadLine());
 
-            Console.WriteLine($"{produto}-{quantidade}R$ {preco}");
-
-            double subTotal = quantidade * preco;
-
-            if (quantidade > 10 && quantidade <= 20)
-            {
-                double desconto = (subTotal / 100 * 10);
-                double totalAPagar = subTotal - desconto;
-                Console.WriteLine($"{produto} Quant: {quantidade} x {preco}");
-                Console.WriteLine($"Sub-total R$: {subTotal}");
-                                Console.WriteLine($"Desconto de 10%: {desconto}");
-                Console.WriteLine($"Valor total a pagar: {totalAPagar}");
-                
-            }else if (quantidade >= 21 && quantidade <= 50)
-            {
-                double desconto = (subTotal / 100 * 20);
-                double totalAPagar = subTotal - desconto;
-                Console.WriteLine($"{produto} Quant: {quantidade} x {preco}");
-                Console.WriteLine($"Sub-total R$: {subTotal}");
-                Console.WriteLine($"Desconto de 20%: {desconto}");
-                Console.WriteLine($"Valor total a pagar: {totalAPagar}");
-
-            }
-            else if (quantidade >= 51 )
-            {
-                double desconto = (subTotal / 100 * 25);
-                double totalAPagar = subTotal - desconto;
-                Console.WriteLine($"{produto} Quant: {quantidade} x {preco}");
-                Console.WriteLine($"Sub-total R$: {subTotal}");
-                Console.WriteLine($"Desconto de 25%: {desconto}");
-                Console.WriteLine($"Valor total a pagar: {totalAPagar}");
-
-            }else
-            {
-                Console.WriteLine($"{produto} Quant: {quantidade} x {preco}");
-                Console.WriteLine($"Sub-total R$: {subTotal}");
-                Console.WriteLine($"Valor total a pagar: {subTotal}");
-
-            }
+            Abatimento.CalcularCompra(produto, quantidade, preco);
         }
     }
 }
